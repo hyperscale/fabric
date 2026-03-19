5a8da24898774acc50d5db4987b214de0e70cb64
Axel Etcheverry <axel@etcheverry.biz>
2026-03-19 17:18:14 +0100

    feat: add eventemitter provider

---
+++ provider/eventemitter/eventemitter_provider_test.go
@@ -0,0 +1,73 @@
+package eventemitter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFactory(t *testing.T) {
+	dispatcher, err := Factory()
+	require.NoError(t, err)
+	assert.NotNil(t, dispatcher)
+}
+
+func TestNewProvider(t *testing.T) {
+	dispatcher, err := Factory()
+	require.NoError(t, err)
+
+	provider := NewProvider(dispatcher)
+	assert.NotNil(t, provider)
+}
+
+func TestProvider_Name(t *testing.T) {
+	dispatcher, _ := Factory()
+	provider := NewProvider(dispatcher)
+
+	assert.Equal(t, ProviderName, provider.Name())
+	assert.Equal(t, "eventemitter", provider.Name())
+}
+
+func TestProvider_Priority(t *testing.T) {
+	dispatcher, _ := Factory()
+	provider := NewProvider(dispatcher)
+
+	assert.Equal(t, 0, provider.Priority())
+}
+
+func TestProvider_Start(t *testing.T) {
+	dispatcher, _ := Factory()
+	provider := NewProvider(dispatcher)
+
+	err := provider.Start()
+	assert.NoError(t, err)
+}
+
+func TestProvider_Stop(t *testing.T) {
+	dispatcher, _ := Factory()
+	provider := NewProvider(dispatcher)
+
+	err := provider.Stop()
+	assert.NoError(t, err)
+}
+
+func TestProvider_StopAfterEmit(t *testing.T) {
+	dispatcher, _ := Factory()
+	provider := NewProvider(dispatcher)
+
+	// Emit an event
+	called := false
+	dispatcher.Subscribe("test", func() {
+		called = true
+	})
+	dispatcher.Dispatch("test")
+
+	err := provider.Stop()
+	assert.NoError(t, err)
+	assert.True(t, called)
+}
+
+func TestProviderName_Constant(t *testing.T) {
+	assert.Equal(t, "eventemitter", ProviderName)
+}
